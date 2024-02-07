2b29cebbc615bbc4fe0f9839a747f658adf72d85
MarkoBrie <marko.briesemann@gmail.com>
2024-02-07 11:43:22 +0100

    fastAPI model test

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # credit_scoring_with_MLOPS
+
+We will create a model to predict credit scores and deploy that model as a WebAPP. We use Github Actions to facilitate DevOps.
+
+Tech Stack:
+**VS Code** — as the IDE of choice.  
+**pipenv** — to handle package dependencies, create virtual environments, and load environment variables.
+**FastAPI** — Python API development framework.
+	**Uvicorn** — ASGI server for FastAPI app.
+**Docker Desktop** — build and run Docker container images on our local machine. (MacOS 11)
+    Containers are an isolated environment to run any code
+**Azure Container Registry** — repository for storing our container image in Azure cloud.
+**Azure App Service** — PaaS service to host our FastAPI app server.
+**Github Actions** — automate continuous deployment workflow of FastAPI app.
+**Streamlit** - Dashboard
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
 import os
+import streamlit as st
 
 # load environment variables
 port = os.environ["PORT"]
 
 # initialize FastAPI
-app = FastAPI()
+app = FastAPI(title="Automatic Credit Scoring",
+              description='''Obtain a credit score (0,1) for ClientID.
+                           Visit this URL at port 8501 for the streamlit interface.''',
+              version="0.1.0",)
 
 @app.get("/")
 def index():
-    return {"data": "Application ran successfully - FastAPI release v4.2 with Github Actions no staging"}
+    return {"data": "Application ran successfully - FastAPI release v4.2 with Github Actions no staging",
+            'prediction': 1,
+            'probability': 0.9
+            
+            }
+    #return {st.title("Hello World")}
   
   
 if __name__ == "__main__":
